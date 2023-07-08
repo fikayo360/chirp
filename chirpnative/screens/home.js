5b095c0a588bf5223e0ab3efaaf90ea42573401e
fikayo <fikayoadele@gmail.com>
2023-07-08 13:34:00 +0100

    testing testing

--- chirpnative/screens/home.js
+++ chirpnative/screens/home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {SafeAreaView,ScrollView} from 'react-native'
 import HomeComponents from '../components/homeComponents'
 import Header from '../components/header'
-//import { newsItems } from '../mockdata/newsitems'
 import axios from "axios";
 import { useState,useEffect } from 'react'
 
@@ -14,6 +13,7 @@ const Home = () => {
     try {
       const response = await axios.get('api/v1/news/getTopStories');
       setNewsItems(response.data.articles);
+      console.log(response.data);
     } catch (error) {
       if (error.response) {
         setError(error.response.data);
--- chirpnative/screens/newscategory.js
+++ chirpnative/screens/newscategory.js
@@ -2,13 +2,30 @@
 import React from 'react'
 import {StyleSheet,SafeAreaView,ScrollView} from 'react-native'
 import NewscategoryItems from '../components/newscategoryItems'
-import {newsItems} from '../mockdata/newsitems'
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import Header from '../components/header'
 
 
 const Newscategory = () => {
-  const [data,setData]= useState(newsItems)
+
+  const [data,setData]= useState([])
+
+  const submit = async () => {
+    try {
+      const response = await axios.get(`api/v1/news/getNewsCategory/${business}`);
+      console.log(response.data.articles);
+      setData(response.data.articles);
+    } catch (error) {
+      if (error.response) {
+        setError(error.response.data);
+      } 
+    }
+  };
+
+ useEffect(() => {
+  submit()
+ },[])
+
   return (
     <SafeAreaView style={styles.container}>
         <Header title={'Category'} />
